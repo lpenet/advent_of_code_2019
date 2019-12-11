8944bf57533ebd2ffc61b625c3a44ef6dfcdc723
Ludovic Pénet <ludovic.penet@dataiku.com>
2019-12-11 02:43:40 +0100

    Day 10, part 2

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -5,6 +5,7 @@ use std::io::{BufRead, BufReader};
 
 extern crate cgmath;
 
+use core::ops::Neg;
 use cgmath::*;
 use cgmath::Vector2;
 
@@ -22,7 +23,10 @@ impl PartialEq for Asteroid {
 
 #[derive(Clone)]
 struct AsteroidField {
-    asteroids: Vec<Asteroid>
+    asteroids: Vec<Asteroid>,
+    canon_index: usize,
+    canon_direction: Vector2::<f64>,
+    former_angle: Option<f64>
 }
 
 impl Asteroid {
@@ -36,7 +40,7 @@ impl AsteroidField {
         let mut asteroids = Vec::<Asteroid>::new();        
         let file = File::open(filename).unwrap();
         let reader = BufReader::new(file);
-
+        let mut canon_index = 0;
         for (y, line) in reader.lines().enumerate() {
             let line = line.unwrap();
             if !line.trim().is_empty() {
@@ -46,13 +50,22 @@ impl AsteroidField {
                             x: x as u32,
                             y: y as u32
                         });
+                    } else if *ch == 'X' as u8 {
+                        asteroids.push(Asteroid {
+                            x: x as u32,
+                            y: y as u32
+                        });
+                        canon_index = asteroids.len() - 1;
                     }
                 }
             }
         }
 
         AsteroidField {
-            asteroids
+            asteroids,
+            canon_index,
+            canon_direction: Vector2::<f64> { x: 0., y: -1. },
+            former_angle: None
         }
     }
 
@@ -81,6 +94,57 @@ impl AsteroidField {
     fn find_best_asteroid(&self) -> (Asteroid, u32) {
         let sights = self.compute_others_in_sight();
         *sights.iter().max_by(|x, y| x.1.cmp(&y.1)).unwrap()
+    }
+
+    fn shoot_asteroid(&mut self) -> Asteroid {
+        let mut displacements = Vec::<(Asteroid,Vector2::<f64>)>::new();
+        let canon_position = self.asteroids[self.canon_index];
+        for a in &self.asteroids {
+            if *a == canon_position {
+                continue;
+            }
+            displacements.push((*a,canon_position.displacement(&a)));
+        }
+
+        println!("canon {:?}", self.canon_direction);
+        match self.former_angle {
+            Some(x) => println!("former angle: {}", x),
+            None => println!("no former angle")
+        }
+        let mut angles_distances: Vec<(Asteroid,f64,f64)> = 
+            displacements.iter().map(|d| (d.0, AsteroidField::modulo(&d.1.angle(self.canon_direction).neg().s), d.1.length()))
+            .collect();
+        angles_distances.sort_by(|a, b| { 
+            if a.1.eq(&b.1) {
+                return a.2.partial_cmp(&b.2).unwrap();
+            } else {
+                return a.1.partial_cmp(&b.1).unwrap();
+            }
+        });
+        angles_distances.reverse();
+        println!("{:?}", angles_distances);
+        match self.former_angle {
+            Some(x) => if angles_distances.len() > 1 { angles_distances.retain(|(aster, angle, distance)| angle.abs() > 1e-2) },
+            None => { }
+        }
+        let shooted = angles_distances.pop().unwrap();
+        self.former_angle = Some(shooted.1);
+        let shooted = shooted.0;
+        self.canon_direction = canon_position.displacement(&shooted);
+        self.asteroids.retain(|i| *i != shooted);
+        self.canon_index = self.asteroids.iter().position(|&aster| aster == canon_position).unwrap();
+        shooted
+    }
+
+    fn modulo(param: &f64) -> f64 {
+        let TWO_PI = 2.*std::f64::consts::PI;
+        if *param > TWO_PI {
+            return *param - TWO_PI;
+        } else if *param < 0. {
+            return *param + TWO_PI;
+        } else {
+            return *param;
+        }
     }
 }
 
@@ -130,12 +194,53 @@ mod tests {
     }
 
     #[test]
-    fn best_in_sighti5() {
+    fn best_in_sight5() {
         let asteroid_field: AsteroidField = AsteroidField::load_file("test5.in");
         let best = asteroid_field.find_best_asteroid();
         assert_eq!(best, (Asteroid { x: 11, y: 13}, 210));
     }
 
+    #[test]
+    fn vaporizei6() {
+        let mut asteroid_field: AsteroidField = AsteroidField::load_file("test6.in");
+        assert_eq!(asteroid_field.asteroids[asteroid_field.canon_index], Asteroid { x: 8, y:3 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 8, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 9, y:0 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 9, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 10, y:0 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 9, y:2 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 11, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 12, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 11, y:2 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 15, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 12, y:2 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 13, y:2 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 14, y:2 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 15, y:2 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 12, y:3 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 16, y:4 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 15, y:4 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 10, y:4 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 4, y:4 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 2, y:4 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 2, y:3 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 0, y:2 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 1, y:2 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 0, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 1, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 5, y:2 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 1, y:0 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 5, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 6, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 6, y:0 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 7, y:0 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 8, y:0 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 10, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 14, y:0 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 16, y:1 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 13, y:3 });
+        assert_eq!(asteroid_field.shoot_asteroid(), Asteroid { x: 14, y:3 });
+    }
 }
 
 impl fmt::Debug for Asteroid {
